064209bc2bc68e4c2f885bf167b2ac088b9a0730
unknown <user@.(none)>
2016-01-07 16:03:52 +0800

    add js

--- hc/ms/views/ms/includes/header.php
+++ hc/ms/views/ms/includes/header.php
@@ -1,13 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-   <meta http-equiv="Content-Type" content="text/html" charset="utf-8">
-   <meta http-equiv="cache-control" content="no-cache,no-store, must-revalidate">
-   <meta http-equiv="pragma" content="no-cache">
-   <meta http-equiv="expires" content="0">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <meta name="description" content="">
-   <meta name="keywords" content="">
-   <meta name="author" content="">
+	<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+	<meta http-equiv="cache-control" content="no-cache,no-store, must-revalidate">
+	<meta http-equiv="pragma" content="no-cache">
+	<meta http-equiv="expires" content="0">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<meta name="author" content="">
 
-   <title></title>
+	<title></title>
+   
+	<link href="<?php echo base_url('assets/css/bootstrap.min.css') ?>" rel="stylesheet">
+   
+	<script src="<?php echo base_url('assets/redactor/redactor.js') ?>"></script>
+	
+	
+	
+                <script src="/roadshow/images/js/fileupload/js/vendor/jquery.ui.widget.js" type="text/javascript"></script>
+                <script src="/roadshow/images/js/load-image.all.min.js" type="text/javascript"></script>
+                <script src="/roadshow/images/js/canvas-to-blob.min.js" type="text/javascript"></script>
+                <script src="/roadshow/images/js/fileupload/js/jquery.iframe-transport.js" type="text/javascript"></script>
+                <script src="/roadshow/images/js/fileupload/js/jquery.fileupload.js" type="text/javascript"></script>
+                <script src="/roadshow/images/js/fileupload/js/jquery.fileupload-process.js" type="text/javascript"></script>
+                <script src="/roadshow/images/js/fileupload/js/jquery.fileupload-image.js" type="text/javascript"></script>
+	
+</head>
+	<body>
+		<div>
